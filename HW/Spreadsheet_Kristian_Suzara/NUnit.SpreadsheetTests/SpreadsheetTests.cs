0dffd2efdd5a6baf14d80da87135de95db1cfaa9
albert <kristian.suzara@wsu.edu>
2021-03-03 07:13:09 -0800

    decided that the first value when creating a spreadsheet should be a column instead of row, created tests for spreadsheet cell values.

--- HW/Spreadsheet_Kristian_Suzara/NUnit.SpreadsheetTests/SpreadsheetTests.cs
+++ HW/Spreadsheet_Kristian_Suzara/NUnit.SpreadsheetTests/SpreadsheetTests.cs
@@ -25,12 +25,50 @@ namespace NUnit.SpreadsheetTests
             // Test if the amount of rows is correct.
             Assert.AreEqual(
                 20,
-                testSpreadSheet.RowCount);
+                testSpreadSheet.ColumnCount);
 
             // Test if the number of columns is correct.
             Assert.AreEqual(
                 5,
-                testSpreadSheet.ColumnCount);
+                testSpreadSheet.RowCount);
+        }
+
+        /// <summary>
+        /// Test for values within the cells of the spreadsheet.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetValue()
+        {
+            Spreadsheet testSpreadSheet = new Spreadsheet(5, 5);
+
+            // Test spreadsheet cell value when empty
+            Assert.AreEqual(
+                string.Empty,
+                testSpreadSheet.GetCell(0, 0).Value);
+
+            // Test spreadsheet cell value after text is set on initially empty cell
+            testSpreadSheet.GetCell(0, 0).Text = "wow";
+            Assert.AreEqual(
+                "wow",
+                testSpreadSheet.GetCell(0, 0).Value);
+
+            // Test spreadsheet cell value after more text is added to a non empty cell
+            testSpreadSheet.GetCell(0, 0).Text += " amazing";
+            Assert.AreEqual(
+                "wow amazing",
+                testSpreadSheet.GetCell(0, 0).Value);
+
+            // Test spreadsheet cell value when it starts with an '=' and is referencing an empty cell
+            testSpreadSheet.GetCell(0, 1).Text += "=B1";
+            Assert.AreEqual(
+                string.Empty,
+                testSpreadSheet.GetCell(0, 1).Value);
+
+            // Test spreadsheet cell value when it starts with an '=' and is referencing a non empty cell
+            testSpreadSheet.GetCell(0, 1).Text += "=A1";
+            Assert.AreEqual(
+                "wow amazing",
+                testSpreadSheet.GetCell(0, 1).Value);
         }
     }
 }
