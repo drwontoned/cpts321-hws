22da9e777f467822dd5180ef4ecccb4859faf2d8
albert <kristian.suzara@wsu.edu>
2021-03-05 19:39:09 -0800

    completed step 5

--- HW/Spreadsheet_Kristian_Suzara/SpreadsheetEngine/Spreadsheet.cs
+++ HW/Spreadsheet_Kristian_Suzara/SpreadsheetEngine/Spreadsheet.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Spreadsheet.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace SpreadsheetEngine
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Spreadsheet class is a 2D array of cell's.
     /// </summary>
@@ -24,7 +29,7 @@ namespace SpreadsheetEngine
         /// </param>
         public Spreadsheet(int rows, int columns)
         {
-            // because 2d arrays are [row, column] it is formatted different from cells which are (column, row)
+            // Because 2D arrays are [row, column] it is formatted different from cells which are (column, row)
             this.spreadsheet = new SpreadsheetCell[rows, columns];
             for (int i = 0; i < rows; i++)
             {
@@ -36,21 +41,54 @@ namespace SpreadsheetEngine
         }
 
         /// <summary>
+        /// Cell property changed event.
+        /// </summary>
+        public event PropertyChangedEventHandler CellPropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Gets number of collumns in Spreadsheet.
+        /// </summary>
+        public int ColumnCount
+        {
+            get
+            {
+                return this.spreadsheet.GetLength(1);
+            }
+        }
+
+        /// <summary>
+        /// Gets number of rows in Spreadsheet.
+        /// </summary>
+        public int RowCount
+        {
+            get
+            {
+                return this.spreadsheet.GetLength(0);
+            }
+        }
+
+        /// <summary>
+        /// Gets Cell based on the column and row that was input into function.
+        /// </summary>
+        /// <param name="column">
+        /// int column is the column index of row from 2D array spreadsheet.
+        /// </param>
+        /// <param name="row">
+        /// int row is the row index of row from 2D array spreadsheet.
+        /// </param>
+        /// <returns>
+        /// Cell based on the row and column index.
+        /// </returns>
+        public Cell GetCell(int column, int row)
+        {
+            return this.spreadsheet[row, column];
+        }
+
+        /// <summary>
         /// Cell class for the spreadsheet.
         /// </summary>
         public class SpreadsheetCell : Cell
         {
-            /// <summary>
-            /// Sets value.
-            /// </summary>
-            /// <param name="value">
-            /// string value Value will be set to.
-            /// </param>
-            public void SetValue(string value)
-            {
-                this.value = value;
-            }
-
             /// <summary>
             /// Initializes a new instance of the <see cref="SpreadsheetCell"/> class.
             /// Constructor for a SpreadsheetCell.
@@ -64,6 +102,17 @@ namespace SpreadsheetEngine
             public SpreadsheetCell(int column, int row)
                 : base(column, row)
             {
+            }
+
+            /// <summary>
+            /// Sets value.
+            /// </summary>
+            /// <param name="value">
+            /// string value Value will be set to.
+            /// </param>
+            public void SetValue(string value)
+            {
+                this.value = value;
             }
         }
     }
